41eccd28cfc47cba3d99d7f0a2c88e5e43c583d3
Giovana Melo <giovanamelo771@outlook.com>
2024-08-27 10:28:41 -0700

    mbsd working

--- DFO.py
+++ DFO.py
@@ -28,11 +28,7 @@ D = np.array([[2, 2], [1, -2]])
 def m(x):
     return x[0]*x[0] + 2*x[1] - 3
 
-#print(m(x))
-
 delta = np.array([m(x + D[:,0]) - m(x), m(x + D[:,1]) - m(x)])
-
-#print(delta.T)
 
 def simplex_gradient(x, D, m):
 
@@ -102,9 +98,11 @@ def gen_simplex_gradient(x, D, p):
 
     for i in range(n):
 
-        row = [p(x+D[:,i]) - p(x)]
+        row = np.array(p(x+D[:,i]) - p(x))
 
         delta.append(row)
+
+    #delta = np.array(delta)
 
     gradient = np.dot(pseudoinverse(D.T), delta)
 
@@ -135,8 +133,6 @@ def gen_simplex_hessian(x, C, D, p):
 
     hessian = np.dot(pseudoinverse(C.T), delta)
 
-    print(hessian)
-
     return hessian
 
 
@@ -150,67 +146,10 @@ D = np.array([[0.1, 0, 0, -0.1, 0], [0, 0.1, -0.1, -0.2, 0.2], [-0.1, 0, 0.1, 0.
 
 D = 0.01 * D
 
-print(gen_simplex_hessian(x, D, D, p))
-
-def mbsd(delta, mi, eta, e_stop, x, D, f, itmax):
-    
-    k = 0
-
-    # MODEL CREATION PHASE
-
-    # using a finite number of points to create a model for f with order-1 gradient accuracy
-
-    D = delta * D
-
-    g = gen_simplex_gradient(x, D, f)
-
-    # MODEL ACCURACY CHECKS
-
-    norm = np.linalg.norm(g,ord = np.inf)
-
-    if delta < e_stop and norm:
-
-        # algorithm succeds and we stop
-
-        return x
-
-    elif delta > mi * norm:
-
-        # model is innacurate so we do the following
-
-        delta = 0.5 * delta
-
-        # keep mi and x as they are
-
-        k = k + 1
-
-    elif delta <= mi * norm:
-
-        # model is acurate and therefore we proceed to the nex step
-
-        #LINE SEARCH
-
-        t = line_search(f,x, g, eta, itmax)[0]
-
-        # We are declaring succes and failure based on the iteration count
-
-        if line_search(f,x, g, eta, itmax)[1] < itmax:
-
-            # update x at iteration k
-
-            x = x - t*g
-
-        else:
-
-            mi = 0.5 * mi
-
-        k = k + 1
+#print(gen_simplex_hessian(x, D, D, p))
 
 
-    return
-
-
-def line_search(f,x, g, eta, itmax):
+def line_search(f, x, g, eta, itmax = 6):
 
     t = 1
 
@@ -218,28 +157,111 @@ def line_search(f,x, g, eta, itmax):
 
     norm = np.linalg.norm(g)
 
-    while f(x - t*g) > f(x) - eta*t*norm and k < itmax:
+    while f(x - (t*g).flatten()) > f(x) - eta*t*norm and k < itmax:
         
         t = 0.5*t
 
         k = k + 1
+
+    print("LINE SEARCH: t = ", t, " iterations = ", k)
     
     return t, k
+
+
+
+def mbsd(delta, mi, eta, e_stop, x, D, f, itmax):
     
+    k = 0
 
-def l(x):
+    stop = False
 
-    return x[0]**2 + 2*x[1]**2
+    # MODEL CREATION PHASE
 
-def grad_l(x):
+    # using a finite number of points to create a model for f with order-1 gradient accuracy
 
-    return np.array([2*x[0], 4*x[1]])
+    while stop == False:
 
-x = np.array([1.0, 2.0])
+        print(" --- ITERATION #", k, " ---")
 
-g = grad_l(x)
+        D = delta * D
 
-print(line_search(l, x, g, 1e-6, 100)[0])
+        g = gen_simplex_gradient(x, D, f)
+
+        # MODEL ACCURACY CHECKS
+
+        norm = np.linalg.norm(g,ord = np.inf)
+
+        print("delta =", delta)
+
+        print("g =", g)
+
+        print("norm = ", norm)
+
+        if delta < e_stop and norm < e_stop:
+
+            # algorithm succeds and we stop
+
+            stop = True
+
+            print("success")
+
+            print(x)
+
+            return x
+
+        elif delta > mi * norm:
+
+            # model is innacurate so we do the following
+
+            delta = 0.5 * delta
+
+            # keep mi and x as they are
+
+            print("model innacurate")
+
+            k = k + 1
+
+        elif delta <= mi * norm:
+
+            print("model accurate and proceed")
+
+            # model is acurate and therefore we proceed to the nex step
+
+            #LINE SEARCH
+
+            print("old_x = ", x)
+
+            t, iterations = line_search(f, x, g, eta, itmax)
+
+            # We are declaring succes and failure based on the iteration count
+
+            if iterations < itmax:
+
+                # update x at iteration k
+
+                x = x - t*g
+
+                print("new_x = ", x)
+
+            else:
+
+                mi = 0.5 * mi
+
+            k = k + 1
+
+    return x
+
+#mbsd(delta, mi, eta, e_stop, x, D, f, itmax)
+
+def r(x):
+
+    return pow(x[0],2) + pow(x[1], 2)
+
+#mbsd(1e-1, 1e-3, 1/2, 1e-4, np.array([1, 2]), np.array([[1,0], [0, 1]]), r, 8)
+
+#mbsd(1e-1, 1e-4, 1/2, 1e-6, np.array([1, 1]), np.array([[1,0], [0, 1]]), r, 8) # SEQUENCE OF MODEL INNACCURATE
+
+#mbsd(1e-2, 1e-3, 1/2, 1e-6, np.array([1, 1]), np.array([[1,0], [0, 1]]), r, 8) # one iteration of line search
 
 
 
--- test_DFO.py
+++ test_DFO.py
@@ -65,13 +65,13 @@ class TestCalc(unittest.TestCase):
 
         for t in range(2):
 
-            self.assertAlmostEqual(DFO.gen_simplex_gradient([0, 0], D, m)[t][0], np.array([[0.8160],[0.4080]])[t][0], places = 8)
+            self.assertAlmostEqual(DFO.gen_simplex_gradient([0, 0], D, m)[t], np.array([[0.8160],[0.4080]])[t][0], places = 8)
 
         D = np.array([[0.02, 0.02, -0.02], [0.01, -0.02, -0.01]])
 
         for t in range(2):
 
-            self.assertAlmostEqual(DFO.gen_simplex_gradient([0, 0], D, m)[t][0], np.array([[0.006666666],[1.986666666]])[t][0], places = 8)
+            self.assertAlmostEqual(DFO.gen_simplex_gradient([0, 0], D, m)[t], np.array([[0.006666666],[1.986666666]])[t][0], places = 8)
 
 
     def test_gen_simplex_hessian(self):
@@ -84,10 +84,13 @@ class TestCalc(unittest.TestCase):
         
         D = np.array([[0.1, 0, 0, -0.1, 0], [0, 0.1, -0.1, -0.2, 0.2], [-0.1, 0, 0.1, 0.2, -0.2]])
 
-        D = 0.001 * D
+        D = 0.0001 * D
 
-        np.testing.assert_allclose(DFO.gen_simplex_hessian(x, D, D, p),np.array([[12, 0, 0], [0, -6, 0], [0, 0, 6]]), atol=1e-03)
+        print(np.linalg.norm(DFO.gen_simplex_hessian(x, D, D, p) - np.array([[12, 0, 0], [0, -6, 0], [0, 0, 6]])))
 
+        np.testing.assert_allclose(DFO.gen_simplex_hessian(x, D, D, p),np.array([[12, 0, 0], [0, -6, 0], [0, 0, 6]]), atol=1e-02)
+
+        
 
         def q(x):
 
@@ -97,9 +100,60 @@ class TestCalc(unittest.TestCase):
         
         D = np.array([[0.1, 0], [0, 0.1]])
 
-        np.testing.assert_allclose(DFO.gen_simplex_hessian(x, D, D, q),np.array([[2, 1], [1, 0]]), atol=1e-08)
+        D = 0.001 * D 
+
+        np.testing.assert_allclose(DFO.gen_simplex_hessian(x, D, D, q),np.array([[2, 1], [1, 0]]), atol=1e-02)
 
 
+        def s(x):
+
+            return np.exp(4*x[0] + 3*(x[1]**2) + 2*(x[2]**3) + x[3]**4)
+        
+        x = np.array([0, 0, 0, 1])
+
+        Ds = np.array([[-0.1, 0, 0, 0], [0.01, -0.1, 0, 0], [0.01, 0.01, -0.1, 0], [0.01, 0.01, 0.01, -0.1]]) 
+
+
+
+
+    def test_line_search(self):
+
+        
+        def k(x):
+            
+            if x >= 1:
+
+                return 1
+            
+            elif x <= 0.88:
+
+                return 1
+            
+            else:
+                
+                return 0
+            
+        # we check the number of iterations taken
+
+        self.assertAlmostEqual(5, DFO.line_search(k, 1, np.array([1]), 1e-6, 10)[1], places = 8)
+
+
+        def n(x):
+
+            if x>= 2.25:
+
+                return 3
+            
+            elif 2 < x < 2.25:
+
+                return -0.5
+            
+            else:
+
+                return 2
+            
+        self.assertAlmostEqual(4, DFO.line_search(n, 2, np.array([-1]), 1e-6, 10)[1], places = 8)
+            
 
 
 
